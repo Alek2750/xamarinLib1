2c22c5ab9f36345c76d7b2f1cdbb93355a87e6d1
Alek2750 <alek2750@gmail.com>
2019-09-11 10:54:09 +0200

    not done

---
+++ Minutes/Minutes/Minutes/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Minutes.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Minutes
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private NoteEntry entry;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            entries.ItemsSource = await App.Entries.GetAllAsync();
+        }
+        private async void OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            NoteEntry item = e.Item as NoteEntry;
+            await Navigation.PushAsync(new NoteEntryEditPage(item));
+        }
+
+
+        public NoteEntryEditPage(NoteEntry entry)
+        {
+            InitializeComponent();
+            this.entry = entry;
+        }
+    }
+}
